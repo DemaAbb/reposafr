d84d5aa446586c81f6c68aa440dec97ee0073a0f
DemaAbb <72205763+DemaAbb@users.noreply.github.com>
2020-12-24 11:58:18 +0300

    Update Models

--- ProjectAfr/Models/Role.cs
+++ ProjectAfr/Models/Role.cs
@@ -10,5 +10,10 @@ namespace ProjectAfr.Models
         public int Id { get; set; }
         public string Name { get; set; }
         
+        public ICollection<User> Users { get; set; }
+        public Role()
+        {
+            Users = new List<User>();
+        }
     }
 }
--- ProjectAfr/Models/User.cs
+++ ProjectAfr/Models/User.cs
@@ -10,5 +10,11 @@ namespace ProjectAfr.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+
+        public ICollection<Role> Roles { get; set; }
+        public User()
+        {
+            Roles = new List<Role>();
+        }
     }
 }
